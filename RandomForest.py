be7d824bb6381396559ab201a3e6ef800cdf822c
fermi_dirac <ManueleBryan@gmail.com>
2017-07-04 13:09:11 -0700

    progress on RandomForest.py

--- RandomForest.py
+++ RandomForest.py
@@ -1,1 +1,14 @@
-#hello world
+import math
+import numpy as np
+
+#data and label length should be the same
+def RandomForest(data, labels, numberOfTrees, minDepth):
+
+
+    #create a bunch of trees and stuff
+    for t in range(0, numberOfTrees):
+        #pick sqrt(n) datapoints. Can be redundant datapoints
+        pickCount = np.floor(np.sqrt(data.size.m))
+        subset = []
+        for i in range(0, pickCount):
+            subset.push(data[:,math.random(data.size.m)])
