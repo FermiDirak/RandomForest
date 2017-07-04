d9f2e14d1ce6ae948652296e729572258af4d6b9
fermi_dirac <ManueleBryan@gmail.com>
2017-07-04 13:19:56 -0700

    check it

--- RandomForest.py
+++ RandomForest.py
@@ -9,6 +9,14 @@ def RandomForest(data, labels, numberOfTrees, minDepth):
     for t in range(0, numberOfTrees):
         #pick sqrt(n) datapoints. Can be redundant datapoints
         pickCount = np.floor(np.sqrt(data.size.m))
+
+        #subset of the dataset to create a tree out of
         subset = []
         for i in range(0, pickCount):
             subset.push(data[:,math.random(data.size.m)])
+
+        #create tree
+
+
+def getSplit(dataset, nFeatures):
+    
