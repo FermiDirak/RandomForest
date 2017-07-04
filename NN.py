a2acf8c91c5b4420bde5f3729e9cdd9c932d4a54
Andrew Huang <sssss465@gmail.com>
2017-07-04 15:18:17 -0700

    Neural Net in tf

---
+++ NN.py
@@ -0,0 +1,18 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+
+"""
+Implementing a simple one hidden layer on the spiral dataset
+"""
+
+if __name__ == '__main__':
+    hello = tf.constant('Hello, TensorFlow!')
+
+    # Start tf session
+    sess = tf.Session()
+
+    # Run the op
+    print(sess.run(hello))
