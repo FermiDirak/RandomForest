e383a2ba27a239edcc88f2afe87453933841968e
FermiDirak <bryan@aermob.com>
2017-07-05 14:10:49 -0700

    moved tree helper functions from RandomForest to DecisionTree

--- RandomForest.py
+++ RandomForest.py
@@ -26,39 +26,5 @@ def RandomForest(data, labels, numberOfTrees, minDepth):
 
     return forest
 
-
-#get the best split point for dataset
-def getRandomSplit(dataset):
-    split = np.transpose(np.matrix(np.zeros(2)))
-    coordN = np.round(np.random.rand())
-    coordM = np.floor(dataset.size.m * np.random.rand())
-
-    split[coordN, 0] = dataset[coordN + 1, coordM]
-
-    return split
-
-
-def getBestGiniSplit(dataset, labelsCount):
-    return 0
-
-
-#calculates gini value of a given dataset
-def calcGini(histogram, labelsCount):
-    gini = 0
-    for i in range(0, histogram.size.m)
-        gini += histogram[0, i] * histogram[0, i]
-    gini = 1 - gini
-    return gini
-
-
-#returns a 1 * labelCount matrix of histogram data
-def getHistogram(dataset, labelsCount):
-    histogram = np.matrix(np.zeros(labelsCount))
-    for i in range(dataset.size.m)
-        j = dataset[0, i]
-        histogram[0, j] += 1
-    return histogram
-
-
 if __name__ == '__main__':
     swag()
