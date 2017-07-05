67557656422f84de67be866d13008b313ae5d7ad
FermiDirak <bryan@aermob.com>
2017-07-05 12:05:48 -0700

    refactored

--- RandomForest.py
+++ RandomForest.py
@@ -1,13 +1,12 @@
 import math
 import numpy as np
 
-from NN import swag #imports 
+from NN import swag #imports
 
 #data and label length should be the same
 def RandomForest(data, labels, numberOfTrees, minDepth):
     numberOfClasses = labels.max()
     data = [labels; data]
-
 
     #create a bunch of trees and stuff
     for t in range(0, numberOfTrees):
@@ -15,17 +14,32 @@ def RandomForest(data, labels, numberOfTrees, minDepth):
         pickCount = np.floor(np.sqrt(data.size.m))
 
         #subset of the dataset to create a tree out of
-        subset = []
+        subset = np.empty([data.size.n, pickCount])
         for i in range(0, pickCount):
-            subset.push(data[:,math.random(data.size.m)])
+            subset[0, i] = data[:, np.floor(data.size.m * np.random.rand())]
 
         #create tree
 
-#get the best split point for dataset
-def getSplit(dataset):
 
-#calculates the entropy of
-def calcEntropy(dataset, split):
+
+#get the best split point for dataset
+def getRandomSplit(dataset):
+    split = np.transpose(np.matrix(np.zeros([0, 0])))
+    coordN = np.round(np.random.rand())
+    coordM = np.floor(dataset.size.m * np.random.rand())
+
+    split[coordN, 0] = dataset[coordN + 1, coordM]
+
+    return split
+
+
+#calculates gini value of a given dataset
+def calcGini(histogram, labelsCount):
+    gini = 0
+    for i in range(0, histogram.size.m)
+        gini += histogram[0, i] * histogram[0, i]
+    gini = 1 - gini
+    return gini
 
 def getHistogram(dataset, labelsCount):
     labels = np.empty([labelsCount, 0])
--- main.py
+++ main.py
@@ -2,28 +2,31 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
-#data generation: creates spiral dataset with 4 classes and 100 samples each
 
 numberOfPoints = 100 #number of data points per class
 numberOfClasses = 4 #number of classes in dataset
 
-data = np.empty([2, numberOfClasses * numberOfPoints])
-
-def display():
+#data generation: creates spiral dataset with 4 classes and 100 samples each
+def generateData(numberOfPoints, numberOfClasses):
+    data = np.empty([3, numberOfClasses * numberOfPoints])
     for i in range(0, numberOfClasses):
+        data[0, i*numberOfPoints:(i+1)*numberOfPoints] = np.matrix(np.ones(numberOfPoints));
         radius = np.linspace(0.05, 1, numberOfPoints)
         theta  = np.linspace(i*2*math.pi/numberOfClasses, i*2*math.pi/numberOfClasses + 3*math.pi/2, numberOfPoints) + \
                  np.random.normal(0, .1, numberOfPoints)
         x = radius * np.cos(theta)
         y = radius * np.sin(theta)
         datum = np.matrix(np.transpose(np.column_stack((x, y))))
-        data[:,i*numberOfPoints:i*numberOfPoints + numberOfPoints] = datum
+        data[1:,i*numberOfPoints:i*numberOfPoints + numberOfPoints] = datum
+    return data
 
+def display(data):
     #displaying classification data
     fig = plt.figure()
     for i in range(0, numberOfClasses):
-        plt.scatter(data[0, i*numberOfPoints:(i+1)*numberOfPoints], data[1,i*numberOfPoints:(i+1)*numberOfPoints])
+        plt.scatter(data[1, i*numberOfPoints:(i+1)*numberOfPoints], data[2,i*numberOfPoints:(i+1)*numberOfPoints])
     plt.show()
 
 if __name__ == '__main__':
-    display()
+    data = generateData(numberOfPoints, numberOfClasses)
+    display(data)
