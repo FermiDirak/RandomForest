657f7b29a194cf964bbadf2278df924097a1b984
FermiDirak <bryan@aermob.com>
2017-07-05 15:12:07 -0700

    added more minor changes :^)

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,8 +1,6 @@
 class Tree: # passing (object ) into class is no longer needed in python3
 
-    def __init__(self, dataset): # oh god u forgot a colon here
-        # not sure what this is interfacing with
-        # Node() object?
+    def __init__(self, dataset):
         self.root = None
         self.left = None
         self.right = None
