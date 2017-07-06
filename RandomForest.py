3eddf7db2c9e104edc68180985ecf073cb3c6ecc
FermiDirak <bryan@aermob.com>
2017-07-05 17:43:34 -0700

    changed variable names to comply with py styleguide

--- RandomForest.py
+++ RandomForest.py
@@ -2,20 +2,20 @@ import math
 import numpy as np
 
 #data and label length should be the same
-def RandomForest(data, labels, numberOfTrees, minDepth):
-    numberOfClasses = labels.max()
+def RandomForest(data, labels, number_of_trees, min_depth):
+    number_of_classes = labels.max()
     data = [labels; data]
 
-    forest = np.empty([0, numberOfTrees])
+    forest = np.empty([0, number_of_trees])
 
     #create a bunch of trees and stuff
-    for t in range(0, numberOfTrees):
+    for t in range(0, number_of_trees):
         #pick sqrt(n) datapoints. Can be redundant datapoints
-        pickCount = np.floor(np.sqrt(data.size.m))
+        pick_count = np.floor(np.sqrt(data.size.m))
 
         #subset of the dataset to create a tree out of
-        subset = np.empty([data.size.n, pickCount])
-        for i in range(0, pickCount):
+        subset = np.empty([data.size.n, pick_count])
+        for i in range(0, pick_count):
             subset[0, i] = data[:, np.floor(data.size.m * np.random.rand())]
 
         #create tree
@@ -26,11 +26,11 @@ def RandomForest(data, labels, numberOfTrees, minDepth):
     return forest
 
 #classifies a 2 featured new instance
-def testPoint(forest, newInstance):
-    histograms = np.empty([1, labelsCount])
+def testPoint(forest, new_instance):
+    histograms = np.empty([1, number_of_classes])
 
     #go down the tree and find histogram of the point for the tree
     for t in range(0, forest.size.m)
         #@TODO find the histogram for a given point for the tree t
 
-    return histograms / forest.size.m
+    return 0
