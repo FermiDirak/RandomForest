f48ef3ceff8d089bed66677f1ef00ab9bb886cf8
FermiDirak <bryan@aermob.com>
2017-07-11 14:59:59 -0700

    deleted for loop in display_training_data

--- main.py
+++ main.py
@@ -36,10 +36,6 @@ def display(data):
 #displays training data for classification
 def display_training_data(data):
     colors = ['red', 'green', 'blue', 'yellow', 'orange']
-
-    #for i in range(0, number_of_classes):
-        #plt.scatter(data[1, i*number_of_points:(i+1)*number_of_points], data[2,i*number_of_points:(i+1)*number_of_points], color=colors[i])
-
     plt.scatter(data[1, :], data[2, :], color=colors, s=40)
 
 def display_decision_boundary(data):
